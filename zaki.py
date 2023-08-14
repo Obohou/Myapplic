ebb2710e0dfd73c1024a6e7a5e1d302d74827e09
zaki <zakaria.obohou@gmail.com>
2023-08-14 11:24:39 +0100

    Joud

---
+++ zaki.py
@@ -0,0 +1,7 @@
+password="zaki"
+while password != "zaki":
+      password=input("enter the password")
+      if password == "zaki":
+        print("the password is correct @")
+      else:
+        print("somthing is wrong!")
